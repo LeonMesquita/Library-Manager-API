17a46b954778aefe07e22e110f5a2adcce5812cb
LeonMesquita <leonmesquita16@gmail.com>
2025-05-24 10:26:02 -0300

    feat: add JWTAuthenticationFilter

---
+++ src/main/java/com/library_manager/api/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.library_manager.api.exceptions;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.io.IOException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler implements AuthenticationFailureHandler  {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType("application/json");
+        response.getWriter().write("{\"error\": \"Authentication failed: " + exception.getMessage() + "\"}");
+    }
+
+
+}
